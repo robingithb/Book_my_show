17e99ad540a468bb3d2eac802509c7c655f1d708
robingithb <robinthomasq007@gmail.com>
2023-04-03 02:18:21 +0530

    building of profile screen,some errors founded

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,11 +1,137 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:socialmedia/modules/post_model.dart';
+import 'package:socialmedia/modules/user_model.dart';
+import 'package:socialmedia/modules/models.dart';
 
 class profilescreen extends StatelessWidget {
   const profilescreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    User? user = ModalRoute.of(context)!.settings.arguments as User?;
+    user = user ??= User.users[0];
+
+    List<Post> post = Post.posts.where((post) {
+      return post.user.id == user!.id;
+    }).toList();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          '@${user.username}',
+          style: Theme.of(context)
+              .textTheme
+              .titleSmall!
+              .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _ProfileInformation(user: user),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ProfileInformation extends StatelessWidget {
+  const _ProfileInformation({
+    super.key,
+    required this.user,
+  });
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 50,
+          backgroundColor: Colors.white,
+          backgroundImage: AssetImage(user.imagePath),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 75.0),
+          child: Row(
+            children: [
+              _buildUserInfo(context, 'following', '${user.following}'),
+              _buildUserInfo(context, 'followers', '${user.followers}'),
+              _buildUserInfo(context, 'Likes', '${user.likes}')
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: const Color(0xffff006e),
+                fixedSize: const Size(200, 50),
+              ),
+              child: Text(
+                'Follow',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                fixedSize: const Size(50, 50),
+              ),
+              child: const Icon(
+                Icons.arrow_drop_down,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  Expanded _buildUserInfo(BuildContext context, String type, String value) {
+    return Expanded(
+      child: Column(
+        children: [
+          Text(
+            value,
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium!
+                .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            type,
+            maxLines: 3,
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall!
+                .copyWith(color: Colors.grey.shade200, letterSpacing: 1.5),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/custom_video_player.dart
+++ lib/widgets/custom_video_player.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:socialmedia/modules/post_model.dart';
+import 'package:socialmedia/screens/profile_screen.dart';
 import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 import '../modules/post_model.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
@@ -35,97 +37,156 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          if (controller.value.isPlaying) {
+    return VisibilityDetector(
+      key: Key(controller.dataSource),
+      onVisibilityChanged: (VisibilityInfo) {
+        if (VisibilityInfo.visibleFraction > 0.5) {
+          controller.play();
+        } else {
+          if (mounted) {
             controller.pause();
-          } else {
-            controller.play();
           }
-        });
+        }
       },
-      child: AspectRatio(
-        aspectRatio: controller.value.aspectRatio,
-        child: Stack(children: [
-          VideoPlayer(controller),
-          const Positioned.fill(
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.black,
-                    Colors.transparent,
-                    Colors.transparent,
-                    Colors.black,
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [0.0, 0.2, 0.8, 1.0],
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (controller.value.isPlaying) {
+              controller.pause();
+            } else {
+              controller.play();
+            }
+          });
+        },
+        child: AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: Stack(children: [
+            VideoPlayer(controller),
+            const Positioned.fill(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black,
+                      Colors.transparent,
+                      Colors.transparent,
+                      Colors.black,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.0, 0.2, 0.8, 1.0],
+                  ),
                 ),
               ),
             ),
-          ),
-          _buildVideoCaption(context),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Container(
-              height: controller.value.size.height,
-              width: MediaQuery.of(context).size.width * 2,
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Material(
-                    color: Colors.transparent,
-                    child: Ink(
-                      decoration: const ShapeDecoration(
-                          color: Colors.black, shape: CircleBorder()),
-                      child: IconButton(
-                        onPressed: () {},
-                        color: Colors.white,
-                        icon: const Icon(Icons.favorite),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ]),
+            _buildVideoCaption(context),
+            _buildVideoActions(context)
+          ]),
+        ),
       ),
     );
   }
 
-  Align _buildVideoCaption(BuildContext context) {
+  Align _buildVideoActions(BuildContext context) {
     return Align(
-      alignment: Alignment.bottomLeft,
+      alignment: Alignment.centerRight,
       child: Container(
-        height: 25,
-        width: MediaQuery.of(context).size.width * 0.75,
-        padding: const EdgeInsets.all(20.0),
+        height: controller.value.size.height,
+        width: MediaQuery.of(context).size.width * 2,
+        padding: const EdgeInsets.only(bottom: 20.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '@${widget.post.user.username}',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleSmall!
-                  .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: const [
+            _videoActions(icon: Icons.favorite, value: '11.2k'),
+            SizedBox(
+              height: 10,
             ),
-            const SizedBox(height: 5),
-            Text(
-              widget.post.caption,
-              maxLines: 3,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodySmall!
-                  .copyWith(color: Colors.white),
+            _videoActions(icon: Icons.comment, value: '5.3k'),
+            SizedBox(
+              height: 10,
             ),
+            _videoActions(icon: Icons.forward_rounded, value: '15.1k'),
           ],
         ),
       ),
+    );
+  }
+
+  GestureDetector _buildVideoCaption(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/profile', arguments: widget.post.user);
+      },
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: Container(
+          height: 25,
+          width: MediaQuery.of(context).size.width * 0.75,
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '@${widget.post.user.username}',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleSmall!
+                    .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                widget.post.caption,
+                maxLines: 3,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall!
+                    .copyWith(color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _videoActions extends StatelessWidget {
+  const _videoActions({
+    super.key,
+    required this.icon,
+    required this.value,
+  });
+
+  final IconData icon;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Material(
+          color: Colors.transparent,
+          child: Ink(
+            decoration: const ShapeDecoration(
+                color: Colors.black, shape: CircleBorder()),
+            child: IconButton(
+              onPressed: () {},
+              color: Colors.white,
+              icon: Icon(icon),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          value,
+          style: Theme.of(context)
+              .textTheme
+              .titleSmall!
+              .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
